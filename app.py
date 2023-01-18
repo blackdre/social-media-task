0b0d3467f283a9801a93abf11dc575a5d3e37a4e
Blessing <blessingshongwe@gmail.com>
2023-01-18 18:22:34 +0200

    added working code to app.py

--- app.py
+++ app.py
@@ -1,9 +1,61 @@
-from flask import Flask
+# import get_data
+from flask import Flask, jsonify
+import concurrent.futures
+import requests
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def social_network_activity():
     # TODO: your code here
-    json_response = {}
-    return json_response
+    """
+        GET /social_network_activity from API endpoints
+    """
+    try:
+        # get tweets from twitter endpoint
+        def get_tweets():
+            twitter = requests.get('https://takehome.io/twitter')
+            return twitter
+
+        # get tweets from facebook endpoint
+        def get_fb_posts():
+            fb = requests.get('https://takehome.io/facebook')
+            return fb
+
+        # get tweets from twitter endpoint
+        def get_ig_posts():
+            ig = requests.get('https://takehome.io/instagram')
+            return ig
+
+    except ValueError:
+        data = jsonify({"message": "Decoding JSON has failed"})
+        return (data)
+
+    def get_api_data():
+
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            f1 = executor.submit(get_tweets)
+            f2 = executor.submit(get_fb_posts)
+            f3 = executor.submit(get_ig_posts)
+
+        elon = get_tweets().json()
+        facebook = get_fb_posts().json()
+        instagram = get_ig_posts().json()
+
+        tweets = len(elon)
+        fb_posts = len(facebook)
+        ig_posts = len(instagram)
+
+        data = {"twitter": tweets,
+                "facebook": fb_posts, "instagram": ig_posts}
+        return data
+
+    # json_response = {
+    #     jsonify(get_api_data())
+    # }
+    return jsonify(get_api_data())
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
--- get_data.py
+++
@@ -1,5 +0,0 @@
-import requests
-import json
-twitter = requests.get(
-    'https://takehome.io/twitter')
-print(twitter.json())
